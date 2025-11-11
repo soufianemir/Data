c102544cbc3eea43d5cdd3fdd8870ca165eb8c6e
Soufiane Mir <soufiane.mir@gmail.com>
2025-11-11 18:04:02 +0000

    a

--- models/Staging/stg_sellers.sql
+++ models/Staging/stg_sellers.sql
@@ -1,9 +1,6 @@
 with src as (
   select
-    seller_id,
-    seller_name,
-    city,
-    country,
+   seller_id, seller_name, grade, hire_date, seniority_days, region, updated_at,
     current_timestamp() as _load_ts
   from {{ source('tp_source','sellers') }}
 )
